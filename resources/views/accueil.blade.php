f4d43ea6e0f6cd3e39955efd01558586b4246736
TotoWolfy <wolfym81@gmail>
2023-04-07 05:32:02 -0700

    premières routes

---
+++ resources/views/accueil.blade.php
@@ -0,0 +1,4 @@
+@extends('index')
+@section('section')
+<h3>{{ "Bienvenue dans ma cuisine !" }}</h3>
+@stop
---
+++ resources/views/liste.blade.php
@@ -0,0 +1,4 @@
+@extends('index')
+@section('section')
+<h3>{{ "Liste des recettes" }}</h3>
+@stop
